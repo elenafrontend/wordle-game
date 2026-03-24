b8446c517f01a4bc560da0b306d458963f45322f
e.gumerova <elenafrontend@ya.ru>
2026-03-24 17:19:23 +0000

    feat: scaffold core components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,16 @@
+import Game from '../Game';
+import Header from '../Header';
+
 function App() {
-  return <div>Wordle</div>;
+  return (
+    <div className="wrapper">
+      <Header />
+
+      <div className="game-wrapper">
+        <Game />
+      </div>
+    </div>
+  );
 }
 
 export default App;
