058534072593833283d85a672731835ee714a11d
e.gumerova <elenafrontend@ya.ru>
2026-03-25 17:06:30 +0000

    refactor: extract letter cell component

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,20 +2,24 @@ import { range } from "../../utils";
 import React from "react";
 import { checkGuess } from "../../game-helpers";
 
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : "cell";
+
+  return <span className={className}>{letter}</span>;
+}
+
 function Guess({ value, answer }) {
-  const info = checkGuess(value, answer);
+  const result = checkGuess(value, answer);
 
   return (
     <p className="guess">
-      {range(5).map((_, index) => {
-        const className = value ? `cell ${info[index].status}` : "cell";
-
-        return (
-          <span className={className} key={index}>
-            {info?.[index].letter}
-          </span>
-        );
-      })}
+      {range(5).map((_, index) => (
+        <Cell
+          key={index}
+          letter={result?.[index].letter}
+          status={result?.[index].status}
+        />
+      ))}
     </p>
   );
 }
